7822ddca835dddd6d3b931ea14d3b280826388ec
Ben Ruwel <benruwel21@gmail.com>
2020-10-07 12:31:12 +0300

    Generate LoginActivity files

---
+++ app/src/main/java/com/appr/digibiz/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.appr.digibiz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.appr.digibiz.R;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
