e68c1f3ec705b9c58e8dc83ba396abf69ecf67d4
Gideon Okuro <gideonollonde@gmail.com>
2020-10-07 12:48:42 +0300

    create the inventory page layout

---
+++ app/src/main/java/com/appr/digibiz/ui/InventoryActivity.java
@@ -0,0 +1,16 @@
+package com.appr.digibiz.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.appr.digibiz.R;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
