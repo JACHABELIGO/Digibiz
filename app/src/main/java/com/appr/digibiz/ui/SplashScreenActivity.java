59d802cc2d37593492e72bb16e0e8e434744e766
Ben Ruwel <benruwel21@gmail.com>
2020-10-07 17:04:22 +0300

    Complete rough design of splash screen

--- app/src/main/java/com/appr/digibiz/ui/SplashScreenActivity.java
+++ app/src/main/java/com/appr/digibiz/ui/SplashScreenActivity.java
@@ -3,13 +3,17 @@ package com.appr.digibiz.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import com.appr.digibiz.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        //we are loading the layout from the manifest file
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
     }
 }
