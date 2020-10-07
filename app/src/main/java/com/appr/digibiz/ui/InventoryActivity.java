fe7c0febecedcc73c139566c69e63deb05d495ac
Gideon Okuro <gideonollonde@gmail.com>
2020-10-07 16:10:44 +0300

    define tablayout, tabitems and viewpager

--- app/src/main/java/com/appr/digibiz/ui/InventoryActivity.java
+++ app/src/main/java/com/appr/digibiz/ui/InventoryActivity.java
@@ -1,12 +1,18 @@
 package com.appr.digibiz.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-
 import com.appr.digibiz.R;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class InventoryActivity extends AppCompatActivity {
+    TabLayout tab_layout;
+    TabItem tab_available;
+    TabItem tab_outOfStock;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
