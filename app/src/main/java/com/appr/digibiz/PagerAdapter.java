fca13d39a01216ae24c846e28c31260e698f7ba7
Gideon Okuro <gideonollonde@gmail.com>
2020-10-07 16:19:20 +0300

    create pager adapter class

---
+++ app/src/main/java/com/appr/digibiz/PagerAdapter.java
@@ -0,0 +1,23 @@
+package com.appr.digibiz;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    public PagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
