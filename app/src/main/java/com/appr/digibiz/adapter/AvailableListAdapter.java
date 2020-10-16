81f755e828aa58e753b0dedec5376f85f1d71d7d
Gideon Okuro <gideonollonde@gmail.com>
2020-10-16 14:05:38 +0300

    implement view holder methods

--- app/src/main/java/com/appr/digibiz/adapter/AvailableListAdapter.java
+++ app/src/main/java/com/appr/digibiz/adapter/AvailableListAdapter.java
@@ -1,4 +1,27 @@
 package com.appr.digibiz.adapter;
 
-public class AvailableListAdapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AvailableListAdapter extends RecyclerView.Adapter<AvailableListAdapter.AvailableViewHolder> {
+    @NonNull
+    @Override
+    public AvailableViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AvailableViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class AvailableViewHolder extends RecyclerView.ViewHolder {
+    }
 }
