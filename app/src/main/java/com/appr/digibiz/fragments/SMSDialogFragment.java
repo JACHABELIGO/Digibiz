bb18427dc6631f62b54b881c184d47ef949bd92f
Ben Ruwel <benruwel21@gmail.com>
2020-10-22 18:51:56 +0300

    Add functionality to send SMS using SMSManager

--- app/src/main/java/com/appr/digibiz/fragments/SMSDialogFragment.java
+++ app/src/main/java/com/appr/digibiz/fragments/SMSDialogFragment.java
@@ -5,14 +5,25 @@ import android.os.Bundle;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.appr.digibiz.R;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class SMSDialogFragment extends DialogFragment {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class SMSDialogFragment extends DialogFragment implements View.OnClickListener {
+    @BindView(R.id.sendMessageButton)
+    Button mSendMessage;
+    @BindView(R.id.reminder_message)
+    TextInputLayout mMessage;
+    @BindView(R.id.reminder_phone)
+    TextInputLayout mPhone;
 
     public SMSDialogFragment() {
         // Required empty public constructor
@@ -31,9 +42,24 @@ public class SMSDialogFragment extends DialogFragment {
     }
 
     @Override
+    public void onClick(View view) {
+        if(view == mSendMessage) {
+            sendSMSMessage();
+        }
+    }
+
+    private void sendSMSMessage() {
+        String message = mMessage.getEditText().getText().toString().trim();
+        String number = mPhone.getEditText().getText().toString().trim();
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number, null, message, null, null);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sms_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_sms_dialog, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }
