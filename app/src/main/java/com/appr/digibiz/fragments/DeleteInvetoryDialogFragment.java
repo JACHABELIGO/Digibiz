37ce5dccacdebfd7b427f89599aa7619b5dc5298
Ben Ruwel <benruwel21@gmail.com>
2020-10-28 12:32:32 +0300

    Remove commented out code and bind views to the fragment

--- app/src/main/java/com/appr/digibiz/fragments/DeleteInvetoryDialogFragment.java
+++ app/src/main/java/com/appr/digibiz/fragments/DeleteInvetoryDialogFragment.java
@@ -5,15 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
 
 import com.appr.digibiz.R;
 import com.appr.digibiz.models.InventoryModel;
+import com.google.android.material.button.MaterialButton;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class DeleteInvetoryDialogFragment extends DialogFragment {
+
+    @BindView(R.id.delete_product_name)
+    TextView mProductName;
+    @BindView(R.id.delete_permanently_btn)
+    MaterialButton mPermanent;
+    @BindView(R.id.delete_transfer_btn)
+    MaterialButton mTransfer;
     private InventoryModel resultInventory;
 
     public DeleteInvetoryDialogFragment() {
@@ -32,18 +44,14 @@ public class DeleteInvetoryDialogFragment extends DialogFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getParentFragmentManager().setFragmentResultListener("key", this, new FragmentResultListener() {
-//            @Override
-//            public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle bundle) {
-//                resultInventory = Parcels.unwrap(bundle.getParcelable("toDeleteDialog"));
-//            }
-//        });
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delete_invetory_dialog, container, false);
+        View view =  inflater.inflate(R.layout.fragment_delete_invetory_dialog, container, false);
+        ButterKnife.bind(this, view);
+        return  view;
     }
 }
